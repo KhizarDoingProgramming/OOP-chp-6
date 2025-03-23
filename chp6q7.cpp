ce9f01247d5b36e74634c54a67f44de06c0c9353
KhizarDoingProgramming <shayadkhizarhogaa@gmail.com>
2025-03-23 23:31:27 +0500

    chp6q7.cpp

---
+++ chp6q7.cpp
@@ -0,0 +1,58 @@
+/*
+--PROGRAM STATEMENT: In ocean navigation, locations are measured in degrees and 
+minutes of latitude and longitude. Thus if you’re lying off the mouth of Papeete 
+Harbor in Tahiti, your location is 149 degrees 34.8 minutes west longitude, and 
+17 degrees 31.5 minutes south latitude. This is written as 149°34.8’ W, 17°31.5’ S. 
+There are 60 minutes in a degree. (An older system also divided a minute into 60 
+seconds, but the modern approach is to use decimal minutes instead.) Longitude is 
+measured from 0 to 180 degrees, east or west from Greenwich, England, to the 
+international dateline in the Pacific. Latitude is measured from 0 to 90 degrees, 
+north or south from the equator to the poles. Create a class angle that includes 
+three member variables: an  int for degrees, a float for minutes, and a char for 
+the direction letter (N, S, E, or W). This class can hold either a latitude 
+variable or a longitude variable. Write one member function to obtain an angle 
+value (in degrees and minutes) and a direction from the user, and a second to 
+display the angle value in 179°59.9’ E format. Also write a three-argument 
+constructor. 
+Write a main() program that displays an angle initialized with the constructor, 
+and then, within a loop, allows the user to input any angle value, and then 
+displays the value. You can use the hex character constant ‘\xF8’, which usually 
+prints a degree (°) symbol.
+*/
+#include <iostream>
+using namespace std;
+
+class Angle {
+public:
+    int degrees;
+    float minutes;
+    char direction;
+
+    void getInput() {
+        cout << "Enter degrees, minutes, and direction (E/W/N/S): ";
+        cin >> degrees >> minutes >> direction;
+        direction = toupper(direction);
+    }
+
+    void display() const {
+        cout << degrees << "\u00B0" << minutes << "' " << direction << endl;
+    }
+};
+
+int main() {
+    Angle a, b{179, 59.5, 'E'};
+    char q = 'y';
+    
+    cout << "Constructor initialized coordinate: ";
+    b.display();
+    
+    while (tolower(q) == 'y') {
+        a.getInput();
+        cout << "Entered coordinate: ";
+        a.display();
+        cout << "Go again (y/n)?: ";
+        cin >> q;
+    }
+    
+    return 0;
+}
